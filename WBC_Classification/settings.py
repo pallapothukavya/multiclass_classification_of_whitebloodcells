6f75b36066d826adb064f64b96ad9da57443bcc4
kavya <kavya@gmail.com>
2026-03-13 21:14:39 +0530

    Fix Railway deployment for Django

--- WBC_Classification/settings.py
+++ WBC_Classification/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-d+f8y8=v++ma6^xlsq_o+%&$q$si6^1%7um-)n2i(xs7om7_un
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
