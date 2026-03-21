24fe7cbe75271adf7045c8a5cdfa8b5136be38cd
kavya <kavya@gmail.com>
2026-03-21 21:07:02 +0530

    fix CSRF trusted origins for replit.app domain

--- WBC_Classification/settings.py
+++ WBC_Classification/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,8 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+]
+
+# Configure CSRF trusted origins for HTTPS hosting environments (Replit, Railway)
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.replit.dev',
+    'https://*.replit.app',
+    'https://*.up.railway.app',
 ]
 
 # Default primary key field type
